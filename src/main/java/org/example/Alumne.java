2f88b897d39b6d3e221d86680451cc7df95a4113
ycazalla1 <ycazaa1@gmail.com>
2025-05-21 08:43:30 +0200

    Versió final

--- src/main/java/org/example/Alumne.java
+++ src/main/java/org/example/Alumne.java
@@ -1,51 +1,104 @@
 package org.example;
 
-//import javax.persistence.*;
+import javax.persistence.*;
 
-//@Entity
+/**
+ * Classe Alumne per gestionar la informació dels alumnes
+ *
+ * @author Yamila, Nikita i Andrea
+ */
+@Entity
 public class Alumne {
-    // @Id
-    private String DNI;
+
+    // Declaració d'atributs
+    @Id
+    private String dni;
     private String nom;
     private String cognom;
 
+    /**
+     * Constructor buït pel JPA
+     */
+    public Alumne(){}
+
+    /**
+     * Constructor de la classe
+     *
+     * @param DNI dni del alumne
+     * @param nom nom de l'alumne
+     * @param cognom cognom de l'alumne
+     */
     public Alumne(String DNI, String nom, String cognom) {
-        this.DNI = DNI;
+        this.dni = DNI;
         this.nom = nom;
         this.cognom = cognom;
     }
 
-    public String getDNI() {
-        return DNI;
+    /**
+     * Mètode per obtenir el dni de l'alumne
+     *
+     * @return DNI de l'alumne
+     */
+    public String getDni() {
+        return dni;
     }
 
-    public void setDNI(String DNI) {
-        this.DNI = DNI;
+    /**
+     * Mètode per establir el dni de l'alumne
+     *
+     * @param dni
+     */
+    public void setDni(String dni) {
+        this.dni = dni;
     }
 
+    /**
+     * Mètode per obtenir el nom de l'alumne
+     *
+     * @return nom de l'alumne
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Mètode per establir el nom de l'alumne
+     *
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    /**
+     * Mètode per obtenir el cognom de l'alumne
+     *
+     * @return cognom de l'alumne
+     */
     public String getCognom() {
         return cognom;
     }
 
+    /**
+     * Mètode per establir el cognom de l'alumne
+     *
+     * @param cognom
+     */
     public void setCognom(String cognom) {
         this.cognom = cognom;
     }
 
+    /**
+     * Mètode toString per mostrar la informació de l'alumne
+     *
+     * @return String amb la informació de l'alumne
+     */
     @Override
     public String toString() {
-        return "Alumne{" +
-                "DNI='" + DNI  +
-                ", nom='" + nom +
-                ", cognom='" + cognom +
+        return "Alumne = " +
+                "DNI: '" + dni + "'" +
+                ", Nom:" + nom +
+                ", Cognom:" + cognom +
                 '}';
     }
-
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,24 +1,45 @@
 package org.example;
 
-//import javax.persistence.*;
+import javax.persistence.*;
 
+/**
+ * Classe Main per obrir la sessió amb ObjectDB i gestionar alumnes
+ *
+ * @author Yamila, Nikita i Andrea
+ */
 public class Main {
     public static void main(String[] args) {
+        // Obrim la sessió amb ObjectDB
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("objectdb:alumnes.odb");
+        EntityManager em = emf.createEntityManager();
 
-        //EntityManagerFactory emf = Persistence.createEntityManagerFactory("objectdb:productes.odb");
-        //EntityManager em = emf.createEntityManager();
+        // Estructura TRY-CATCH per gestionar excepcions
+        try{
+            // Inciem la transacció i afegim alumnes
+            em.getTransaction().begin();
+            em.persist(new Alumne("12345678A", "Mauri", "Hidalgo"));
+            em.persist(new Alumne("14559533P", "Fernando", "Alonso"));
+            em.persist(new Alumne("12345678B", "Yamila", "Cazalla"));
+            em.persist(new Alumne("12345678C", "Nikita", "Matvei"));
+            em.persist(new Alumne("12345678D", "Andrea", "Esplugas"));
+            em.persist(new Alumne("12345678E", "Juan", "Cuesta"));
+            em.persist(new Alumne("12345678F", "Max", "Verstappen"));
+            em.persist(new Alumne("12345678G", "Carlos", "Sainz Jr."));
 
-        //em.getTransaction().begin();
-        //em.persist(new Alumne("12345678A", "Nom", "Cognom"));
-        //em.getTransaction().commit();
+            // Finalitzem la transacció (commit)
+            em.getTransaction().commit();
 
-        //TypedQuery<Alumne> query = em.createQuery("SELECT a FROM Alumne a", Alumne.class);
-        //for (Alumne a : query.getResultList()) {
-        //    System.out.println(s);
-        //}
+            // Consultem i mostrem els alumnes
+            TypedQuery<Alumne> query = em.createQuery("SELECT a FROM Alumne a", Alumne.class);
+            for (Alumne a : query.getResultList()) {
+                System.out.println(a);
+            }
+        } catch(Exception e){
+            System.err.println(e.getMessage());
+        }
 
-        //em.close();
-        //emf.close();
-
+        // Tancament de la sessió i del EntityManagerFactory
+        em.close();
+        emf.close();
     }
 }
